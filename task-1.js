bc14b554bb9efeb3cdbf0e2bf4ff6d1d2b7f8a08
tucesenel55 <tcsrdr55@gmail.com>
2024-12-05 02:45:27 +0300

    task-1 created

---
+++ task-1.js
@@ -0,0 +1,39 @@
+let productsSize=0
+
+function isEnoughCapacity(products){
+
+    for(i=0; i<products.length; i++ ){
+      const  productsSize = productsSize+Object.products(values)
+        //  return productsSize
+         console.log(productsSize);
+
+    }
+
+}
+    isEnoughCapacity({apples:2,grapes:4,carrots:8 })
+
+
+//     if(productsSize<=containerSize){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(
+//     isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+//   ); // true
+
+
+//   console.log(
+//     isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+//   ); // false
+
+//   console.log(
+//     isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+//   ); // true
+
+//   console.log(
+//     isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+//   ); // false

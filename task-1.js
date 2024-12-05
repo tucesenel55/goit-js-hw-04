fef234f9817f256690b04c13e8a0c3a90db6326d
tucesenel55 <tcsrdr55@gmail.com>
2024-12-05 16:57:47 +0300

    task 1 completed

--- task-1.js
+++ task-1.js
@@ -1,39 +1,37 @@
-let productsSize=0
+function isEnoughCapacity(products, containerSize){
+ 
+  const values=Object.values(products)
 
-function isEnoughCapacity(products){
+  let sum=0
+  
+  for(let i=0;i<values.length;i++){
+  
+    sum+=values[i]
+  }
 
-    for(i=0; i<products.length; i++ ){
-      const  productsSize = productsSize+Object.products(values)
-        //  return productsSize
-         console.log(productsSize);
 
-    }
+  if(sum<=containerSize){
+    return true
+  }
 
+  else{
+    return false
+  }
 }
-    isEnoughCapacity({apples:2,grapes:4,carrots:8 })
 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
-//     if(productsSize<=containerSize){
-//         return true
-//     }
-//     else{
-//         return false
-//     }
-// }
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
 
-// console.log(
-//     isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-//   ); // true
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
 
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
 
-//   console.log(
-//     isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-//   ); // false
-
-//   console.log(
-//     isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
-//   ); // true
-
-//   console.log(
-//     isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
-//   ); // false

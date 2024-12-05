fa2e4062f1aa17c44635087043d2fcfb5077c283
tucesenel55 <tcsrdr55@gmail.com>
2024-12-06 01:11:11 +0300

    console log tests added to tasks

--- js/task-3.js
+++ js/task-3.js
@@ -3,15 +3,23 @@ const profile = {
   playTime: 300,
   changeUsername(newName) {
     this.username = newName;
-    return this.username;
+    
   },
 
   updatePlayTime(saatler) {
     this.playTime = this.playTime + saatler;
-    return this.playTime;
+   
   },
 
   getInfo() {
     return `${this.username} has ${this.playTime} active hours!`;
   },
 };
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
